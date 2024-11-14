e81d74e467ec45ea35db7c55cfec54ede841d4ba
Eva Gunawan <gunawanevaa@gmail.com>
2024-11-14 08:45:45 -0600

    Changed method of determining if empty since file name impacts byte size in os.path.getsize

--- nf_core/bin/file_size_check.py
+++ nf_core/bin/file_size_check.py
@@ -5,12 +5,13 @@ import os
 import sys
 import logging
 import pandas as pd
+import gzip
 
 logging.basicConfig(level = logging.INFO, format = '%(levelname)s : %(message)s', force = True)
 
 def parse_args(args=None):
     description= 'Checks to ensure the file has content. Removes the file from the samplesheet if there is no data.'
-    epilog = 'Example usage: python3 file_size_check.py <SAMPLE_SHEET>'
+    epilog = 'Example usage: python3 file_size_check.py <SAMPLESHEET>'
     parser = argparse.ArgumentParser(description=description, epilog=epilog)
     parser.add_argument('samplesheet',
         help='Samplesheet to check.')
@@ -25,44 +26,43 @@ def get_file_path(samplesheet):
     file_path = df[column_name].iloc[1]
     path_only = os.path.dirname(file_path)
 
-    logging.debug("Setting path up")
+    logging.debug("Setting full path up")
     path_only = path_only + "/"
 
     return path_only
 
 def check_file_bytes(file_path):
 
-    logging.debug("Setting up empty list and dictionary.")
-    file_size_dict = {}
-    files_to_check = []
+    with gzip.open(file_path, 'rb') as infile:
 
-    for filename in os.listdir(file_path):
-        logging.debug(f"Processing {filename}.")
-        path = file_path + filename
-        size = os.path.getsize(path)
-        file_size_dict[filename] = size
+        logging.debug(f"Try to read one byte from {infile}")
+        try:
 
-    for k,v in file_size_dict.items():
-        if int(v) <= 20:
-            files_to_check.append(k)
+            logging.debug(f"{infile} is empty")
+            return infile.read(1) == b''
 
-    return files_to_check
+        except OSError:
+            logging.info(f"Error reading file: {file_path}")
+            return True
+
 
 def remove_from_samplesheet(list_files, samplesheet):
 
     outfile = "final_" + os.path.basename(samplesheet)
+    removed = "removed_samples_" + os.path.basename(samplesheet)
 
-    with open(samplesheet, "r") as file, open(outfile, "w") as out:
+    with open(samplesheet, "r") as file, open(outfile, "w") as out, open(removed, "w") as rm:
         for line in file:
             if any(file in line for file in list_files):
-                pass
+                rm.write(line)
             else:
                 out.write(line)
 
-    return out
+    return out, rm
 
 def main(args=None):
     args = parse_args(args)
+
     file_path = get_file_path(args.samplesheet)
     files_to_check_bytes = check_file_bytes(file_path)
     remove_from_samplesheet(files_to_check_bytes, args.samplesheet)
